3eaa1f5ab97378dbb6ace60e0aec5628982b7d31
jesse <jesse_002@163.com>
2024-04-15 08:37:21 +0800

    wasm types

---
+++ src/type/ImportDType.hpp
@@ -0,0 +1,13 @@
+#ifndef _wasm_import_d_type_
+#define _wasm_import_d_type_
+#include <stdint.h>
+enum class ImportDType : uint8_t
+{
+    FUNC = 0U,
+    TABLE = 1U,
+    MEM = 2U,
+    GLOBAL = 3U
+};
+
+
+#endif
