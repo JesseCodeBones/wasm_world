1de361bd60c34d3f8621d02aad0d14d376e63628
jesse <jesse_002@163.com>
2024-04-24 07:34:51 +0800

    fix ci

--- acceptance-test/test.py
+++ acceptance-test/test.py
@@ -1,17 +1,22 @@
 import os
-import subprocess
+import subprocess, sys
 
 # 获取 script 文件夹下所有 .wasm 文件
-folder_path = "../scripts/"
+current_directory = os.getcwd()
+folder_path = os.path.join(current_directory, "../scripts/")
+executable_path = os.path.join(current_directory, "../build/wasm-interpreter")
 wasm_files = [f for f in os.listdir(folder_path) if f.endswith('.wasm')]
+runnable = os.path.abspath(executable_path)
+if sys.platform.startswith('win'):
+    runnable += ".exe"
 for index, wasm_file in enumerate(wasm_files):
     # 获取对应的 .log 文件名
     log_file = wasm_file.replace('.wasm', '.log')
-    log_file = os.path.join('../scripts/', log_file)
+    log_file = os.path.join(current_directory, '../scripts/', log_file)
     if os.path.exists(log_file):
         # 执行 wasm 文件，获取其标准输出
         print(f"executing test {str(index)} / {len(wasm_files)} - {wasm_file} ... ")
-        result = subprocess.run(['../build/wasm-interpreter', os.path.join(folder_path, wasm_file)], capture_output=True, text=True)
+        result = subprocess.run([runnable, os.path.join(folder_path, wasm_file)], capture_output=True, text=True)
         output = result.stdout
         print(f"script output: {output}")
 
