af88b2164c0025e589b71c3ed064517a06d887a8
JesseChen <56120624+JesseCodeBones@users.noreply.github.com>
2024-09-25 17:33:02 +0800

    Release build also can trace execution (#36)
    * feat
    
    * remove NDEBUG check
    
    ---------
    
    Co-authored-by: Jesse Chen <48510049@qq.com>

--- src/CompilerConstrant.hpp
+++ src/CompilerConstrant.hpp
@@ -5,19 +5,11 @@ namespace CONFIG {
 inline bool WASM_DEBUG_OPTION;
 }
 
-#ifdef NDEBUG
-// release compiler donot have trace functionality
-#define WASM_DEBUG(MSG)                                                        \
-  do {                                                                         \
-  } while (0)
-#else
 #define WASM_DEBUG(MSG)                                                        \
   do {                                                                         \
     if (CONFIG::WASM_DEBUG_OPTION) {                                           \
       std::cout << "[WASM COMPILER] [TRACE]:\t" << MSG;                        \
     }                                                                          \
   } while (0)
-
-#endif
 
 #endif
