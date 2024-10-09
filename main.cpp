b21368ab40e1b47f5cf5555ba9b03990671676d2
Jesse Chen <ruixiang.chen@bizops.com.cn>
2024-10-09 09:54:46 +0800

    uvwasi args get

--- main.cpp
+++ main.cpp
@@ -9,18 +9,48 @@
 #include "src/reader/ModuleReader.hpp"
 #include "src/runtime/SampleRuntime.hpp"
 
+#include <cstdlib>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include "uvwasi.h"
+
 extern bool CONFIG::WASM_DEBUG_OPTION;
 
 int main(int argc, char **argv) {
 
-  // std::filesystem::path filePath =
-  //     "/home/jesse/workspace/wasm_world/scripts/7global.wasm";
+  uvwasi_t uvwasi;
+  uvwasi_options_t init_options;
+  uvwasi_errno_t err;
+
+  /* Setup the initialization options. */
+  init_options.in = 0;
+  init_options.out = 1;
+  init_options.err = 2;
+  init_options.fd_table_size = 3;
+  init_options.envp = NULL;
+  init_options.preopenc = 1;
+  init_options.preopens =
+      (uvwasi_preopen_t *)calloc(1, sizeof(uvwasi_preopen_t));
+  init_options.preopens[0].mapped_path = "/var";
+  init_options.preopens[0].real_path = ".";
+  init_options.allocator = NULL;
+  init_options.preopen_socketc = 0;
   CONFIG::WASM_DEBUG_OPTION = false;
-  // loop args
+  std::vector<std::string> wasmArgs;
   for (int i = 0; i < argc; i++) {
     if (std::string_view(argv[i]) == "--debug") {
       CONFIG::WASM_DEBUG_OPTION = true;
+    } else if (std::string_view(argv[i]).starts_with("-D")) {
+      std::string_view define(argv[i]);
+      define.remove_prefix(2);
+      wasmArgs.emplace_back(define);
     }
+  }
+  init_options.argc = wasmArgs.size();
+  init_options.argv = (const char **)calloc(init_options.argc, sizeof(char *));
+  for (int i = 0; i < wasmArgs.size(); i++) {
+    init_options.argv[i] = wasmArgs[i].c_str();
   }
 
   WASM_DEBUG("\b\b\b\bRunning: " << argv[argc - 1] << "\n");
@@ -33,5 +63,9 @@ int main(int argc, char **argv) {
   SampleRuntime::registerRuntime(module);
   ModuleReader moduleReader(std::move(fileContents), module);
   moduleReader.prepareModule();
+  /* Initialize the sandbox. */
+  err = uvwasi_init(&uvwasi, &init_options);
+  module.uvwasi = &uvwasi;
   module.execute();
+  uvwasi_destroy(&uvwasi);
 }
